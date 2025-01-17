d600184a04e59039d928e97ef20ca87040cebd05
coixo <vivansingh8372@outlook.com>
2025-01-18 01:31:32 +0530

    Update __init__.py

--- Royalkifeelings/__init__.py
+++ Royalkifeelings/__init__.py
@@ -1,29 +1,31 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 from pytgcalls import PyTgCalls, idle
 from Royalkifeelings.callmusic.config import API_HASH, API_ID, BOT_TOKEN, OWNER_ID, SESSION_NAME
 from Royalkifeelings.plugins import ALL_PLUGINS
 
+# PYROGRAM CLIENT 1
 bot = Client(
-    "Royalkifeelings",
+    name="Royalkifeelings1",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
 )
 
+# PYROGRAM CLIENT 2
 Royalboyamit = Client(
+    name="Royalkifeelings2", 
     api_id=API_ID,
     api_hash=API_HASH,
-    session_name=SESSION_NAME,
+    session_string=SESSION_NAME,
 )
 
-user = PyTgCalls(
-    Royalboyamit,
-    cache_duration=100,
-    overload_quiet_mode=True,
-)
+# Py-tgcalls CLIENT 1
+user = PyTgCalls(Royalboyamit)
 
-call_py = PyTgCalls(Royalboyamit, overload_quiet_mode=True)
+# Py-tgcalls CLIENT 2
+call_py = PyTgCalls(Royalboyamit)
 
+# CONFIGURE
 OWNER_NAME = "Royal_boy_amit"
 F_OWNER = OWNER_ID[0]
 
