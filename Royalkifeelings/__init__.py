204e331ce2f328934cc3926631ed8b5d193b1e78
coixo <vivansingh8372@outlook.com>
2025-01-18 02:52:21 +0530

    Update __init__.py

--- Royalkifeelings/__init__.py
+++ Royalkifeelings/__init__.py
@@ -9,7 +9,7 @@ bot = Client(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    ) 
+) 
 
 # PYROGRAM CLIENT 2
 Royalboyamit = Client(
@@ -17,25 +17,10 @@ Royalboyamit = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=SESSION_NAME,
-    )
+)
 
 # Py-tgcalls CLIENT 1
 user = PyTgCalls(Royalboyamit)
 
 # Py-tgcalls CLIENT 2
 call_py = PyTgCalls(Royalboyamit)
-
-# CONFIGURE
-OWNER_NAME = "Royal_boy_amit"
-F_OWNER = OWNER_ID[0]
-
-with Client("Royalkifeelings", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
-    x = app.get_me()
-    BOT_ID = x.id
-    BOT_NAME = x.first_name + (x.last_name or "")
-    BOT_USERNAME = x.username
-    BOT_MENTION = x.mention
-    BOT_DC_ID = x.dc_id
-with Royalboyamit as ass:
-    getass = ass.get_me()
-    ASSISTANT_USERNAME = getass.username
